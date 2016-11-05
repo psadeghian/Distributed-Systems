57871bd25ba6c477fa0855b5c3ebd0b3d984dfc8
psadeghian <Pasha@Pashas-Mac.local>
2016-11-05 19:33:03 -0400

    Added Get and Remove functionality

--- Distributed-File-System/Server1.py
+++ Distributed-File-System/Server1.py
@@ -1,4 +1,5 @@
 from xmlrpc.server import SimpleXMLRPCServer
+import xmlrpc.client, sys, os
 
 HOST = "0.0.0.0"
 PORT = 6001
@@ -9,8 +10,15 @@ class RemoteFunctions:
     def put_on_server(self, encoded):
         with open("temp/temp.txt", "wb") as handle:
             handle.write(encoded.data)
-            return True
+            return "Done"
 
+    def return_get(self, file_addr):
+        with open(file_addr, "rb") as handle:
+            return xmlrpc.client.Binary(handle.read())
+
+    def remove(self,path):
+        os.remove(path)
+        return "File removed"
 server.register_instance(RemoteFunctions())
 print("Server 1 is listening at " + str(server.server_address[0])+ ":" + str(server.server_address[1]))
 server.serve_forever()
